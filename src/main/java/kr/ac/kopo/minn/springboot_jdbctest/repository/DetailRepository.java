932d62c7d70915881ead506a8922d44cc56ac94a
kjminn <kjminn2001@gmail.com>
2025-09-22 09:51:55 +0900

    @OneToOne 01

--- src/main/java/kr/ac/kopo/minn/springboot_jdbctest/repository/DetailRepository.java
+++ src/main/java/kr/ac/kopo/minn/springboot_jdbctest/repository/DetailRepository.java
@@ -1,10 +1,11 @@
 package kr.ac.kopo.minn.springboot_jdbctest.repository;
 
+import kr.ac.kopo.minn.springboot_jdbctest.domain.Detail;
 import kr.ac.kopo.minn.springboot_jdbctest.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DetailRepository extends JpaRepository<Product, Integer> {
+public interface DetailRepository extends JpaRepository<Detail, Integer> {
 
 }
